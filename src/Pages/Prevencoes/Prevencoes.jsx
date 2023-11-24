8791a4d10569d824c8330161e9741aff6e1196fc
vinicius-prates <viniprates2211@gmail.com>
2023-11-24 13:50:05 -0300

    panding  on prevencoes

--- src/Pages/Prevencoes/Prevencoes.jsx
+++ src/Pages/Prevencoes/Prevencoes.jsx
@@ -2,8 +2,28 @@ import { Navbar } from "../../Components/Navbar/Navbar"
 
  export const Prevencoes = () => {
     return(
-        <div>
+        <div className="prevencoes-main-container">
             <Navbar/>
+            <div className="prevencoes-container">
+                <div className="prevencoes-tipos">
+                    <h1>PREVENÇÃO E TRATAMENTO DE DOENÇAS NÃO TRANSMISSÍVEIS</h1>
+                    <ul>
+                        <h3>Melhoria da Qualidade de Vida:</h3>
+                        <li>Prevenção: Adotar hábitos saudáveis reduz o risco de desenvolver doenças não transmissíveis.</li>
+                        <li>Tratamento: Intervenções eficazes melhoram a qualidade de vida dos afetados.</li>
+                    </ul>
+                    <ul>
+                        <h3>Redução de Fatores de Risco:</h3>
+                        <li>Prevenção: Identificar e reduzir fatores de risco, como tabagismo e dieta inadequada.</li>
+                        <li>Tratamento: Controlar fatores de risco ajuda na gestão contínua da doença.</li>
+                    </ul>
+                    <ul>
+                        <h3>Promoção da Conscientização:</h3>
+                        <li>Prevenção: Educar a população sobre a importância da prevenção.</li>
+                        <li>Tratamento: Conscientizar sobre a gestão e o controle eficaz das doenças.</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
  }
